4c42bb043ccd59ddaaaf6a9f45ba6a21eac140ba
istarnion <hallgeir@loekken.org>
2020-01-23 21:09:51 +0100

    Flesh out the Tiled map class

--- src/tiled_map.js
+++ src/tiled_map.js
@@ -1,24 +1,58 @@
+import { images } from "./graphics.js";
+
+// Reference: https://doc.mapeditor.org/en/stable/reference/json-map-format/
+
 function createTileset(ts) {
-    return new Tileset();
+    return new Tileset(ts.name, ts.tilewidth, ts.tileheight, ts.margin, ts.spacing);
 }
 
 export class TiledMap {
     constructor(map_json) {
         const map = JSON.parse(map_json);
-        this.tileset = createTileset(map.tilesets[0]);
 
+        this.properties = {};
+        for(const prop of map.properties) {
+            this.properties[prop.name] = prop.value;
+        }
+
+        this.tileset = createTileset(map.tilesets[0]);
         this.width = map.width;
         this.height = map.height;
-        this.tile_size = 32;
+        this.tile_width = this.tileset.width;
+        this.tile_height = this.tileset.height;
         this.layers = [];
+        this.objects = {};
 
         for(const l of map.layers) {
-            const layer = {
-                tiles: [],
-                properties: {}
-            };
+            if(l.type === "tilelayer") {
+                const layer = {
+                    tiles: l.data,
+                    properties: {}
+                };
 
-            this.layers.push(layer);
+                for(const prop of l.properties) {
+                    layer.properties[prop.name] = prop.value;
+                }
+
+                this.layers.push(layer);
+            }
+            else if(l.type === "objectgroup") {
+                const group = {
+                    objects: {},
+                    properties: {}
+                };
+
+                for(const obj of l.objects) {
+                    group.objects[obj.name] = obj;
+                }
+
+                for(const prop of l.properties) {
+                    group.properties[prop.name] = prop.value;
+                }
+
+                this.objects[l.name] = group;
+            }
+            else console.assert("Unsupported Tiled map layer type '"+l.type+"'");
         }
     }
 
@@ -27,7 +61,7 @@ export class TiledMap {
             for(var y=0; y<this.height; ++y) {
                 for(var x=0; x<this.width; ++x) {
                     const index = x + y * this.width;
-                    this.tileset.drawTile(layer.tiles[i], x*this.tile_size, y*this.tile_size);
+                    this.tileset.drawTile(layer.tiles[i], x*this.tile_width, y*this.tile_height);
                 }
             }
         }
