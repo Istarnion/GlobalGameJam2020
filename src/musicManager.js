5ae41dc28bfbc97fa6d4c10dc35a80a8a66b799b
Ole Jørgen Skogstad <skogstad@protonmail.com>
2020-02-01 10:59:09 +0100

    Add stupid functions for switching music
    No cross fading

--- src/musicManager.js
+++ src/musicManager.js
@@ -3,13 +3,40 @@ import { Howl } from 'howler';
 class MusicManager {
     constructor() {
         this.music = {}
-        this.music.startDrone = new Howl({
-                src: ['res/sounds/Menu_Loop.mp3'],
-                loop: true,
-                onload: () => {
-                    this.music.startDrone.play();
-                }
+
+        this.music.menuLoop = new Howl({
+            src: ['res/sounds/Menu_Loop.mp3'],
+            loop: true,
         });
+
+        this.music.gameLoop = new Howl({
+            src: ['res/sounds/Game_Loop.mp3'],
+            loop: true,
+        });
+
+        this.music.creditsLoop = new Howl({
+            src: ['res/sounds/Credits_Loop.mp3'],
+            loop: true,
+        });
+
+        this.setGameState();
+    }
+
+    setMenuState() {
+        this.music.gameLoop.stop();
+        this.music.menuLoop.play();
+    }
+
+    setGameState() {
+        this.music.creditsLoop.stop();
+        this.music.menuLoop.stop();
+        this.music.gameLoop.play();
+    }
+
+    setCreditsState() {
+        this.music.menuLoop.stop();
+        this.music.gameLoop.stop();
+        this.music.creditsLoop.play();
     }
 
     update(deltaTime) {
